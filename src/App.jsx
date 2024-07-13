26ebf03b88579284b5ddcfb65cc70dfa556aa90c
ubasjunril11 <ubasjunril11@gmail.com>
2024-07-13 11:35:58 +0800

    wow

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1>
+      First React App Installation
+     </h1>
+     <h2>
+      What is react?
+     </h2>
+     <p>React is a free and open-source front-end Javascript library for building user interfaces based
+     for building user interfaces based on components by facebook Inc.<br/>
+    It is maintained by Meta and community of individual developers and companies.<br/>
+    React can be used to develop single-page, mobile, or server-rendered applications with frameworks like Next.js.<br/>
+     </p>
+      1. Tormis, Joma Lay M.<br/>
+      2. Rebojo, Orencio Jr.<br/>
+    </div>
+  )
+}
+
+export default App
